b1d31521a8f1e44fab174645b0a8d8bbeb3a3a51
manbow1206 <tyasunaga@iyell.jp>
2021-11-01 21:50:56 +0900

    [add]add "ref method"

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { reactive } from "vue";
+import { ref } from "vue";
 
 createApp(App).use(store).use(router).mount("#app");
 
+// reactive method
+// object only
 const state = reactive({
   count: 0,
 });
 console.log(state);
+
+// ref method
+// primitive
+const countRef = ref(0);
+console.log(countRef);
